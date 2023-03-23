b0ddf284ddf19f4e6ca114472ff8929397df9e47
simon <simonkhartoum@gmail.com>
2023-03-23 18:03:36 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let add = document.querySelector('form')
+  add.addEventListener('submit', (event)=>{
+    event.preventDefault();
+   buildToDo(event.target.new.value);
+   add.reset()
+  })
+  function buildToDo(todo){
+    let p = document.createElement('li');
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x';
+    p.textContent = `${todo} `
+    document.querySelector('ul').appendChild(p)
+    p.appendChild(btn)
+  }
+  function handleDelete(e){
+e.target.parentNode.remove()
+  }
+ 
 });
